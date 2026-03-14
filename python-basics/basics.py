e4486499e8a682cc16da51cc88ff220e39fa46a5
sukhraj sandhar <sandharsukhraj5@gmail.com>
2026-03-13 22:00:28 -0700

    python basics

---
+++ python-basics/basics.py
@@ -0,0 +1,45 @@
+name = "John Doe"
+age = 25
+print("My favorite colors are", "blue", "green", "red")
+
+
+my_integer_var = 10
+print("Integer:", my_integer_var)
+
+my_float_var = 4.50
+print("Float:", my_float_var)
+
+my_string_var = "hello"
+print("String:", my_string_var)
+
+my_boolean_var = True
+print("Boolean:", my_boolean_var)
+
+my_set_var = {7, "hello", 8.5}
+print("Set:", my_set_var)
+
+my_dictionary_var = {"name": "Alive", "age": 25}
+print("Dictionary:", my_dictionary_var)
+
+my_tuple_var = (7, "hello", 8.5)
+print("Tuple:", my_tuple_var)
+
+my_range_var = range(5)
+print("Range:", my_range_var)
+
+my_list = [22, "Hello world", 3.14, True]
+print(my_list)
+
+my_none_var = None
+print("None:", my_none_var)
+
+my_var_1 = "Hello world"
+my_var_2 = 21
+
+print(type(my_var_1))
+print(type(my_var_2))
+
+isinstance("Hello world", str)
+isinstance(True, bool)
+isinstance(42, int)
+isinstance("John Doe", int)
