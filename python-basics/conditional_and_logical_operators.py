ee1fee30b1e177cc6c60eddb45ffd17cdf22215e
sukhrajsandhar <sandharsukhraj5@gmail.com>
2026-03-21 22:58:29 -0700

    Conditional and Logical Operators

--- python-basics/conditional_and_logical_operators.py
+++ python-basics/conditional_and_logical_operators.py
@@ -1,2 +1,16 @@
 # Conditional statements, or conditionals, let you control the flow of your program based on whether certain conditions are true or false.
-#
+
+# == Equal
+# ! = Not equal
+# > Greater than
+# < Less than
+# > = Greater than or equal
+# < = Less than or equal
+
+print(3 > 4)  # False
+print(3 < 4)  # True
+print(3 == 4)  # False
+print(4 == 4)  # True
+print(3 != 4)  # True
+print(3 >= 4)  # False
+print(3 <= 4)  # True
