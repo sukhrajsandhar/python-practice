fb7c1b5aeca30f58358e8e53f8dc88af5c9240ec
sukhrajsandhar <sandharsukhraj5@gmail.com>
2026-03-27 20:10:05 -0700

    Loops and Sequences Python

---
+++ python-basics/Loops_and_Seq.py
@@ -0,0 +1,13 @@
+cities = ["Los Angeles", "London", "Tokyo"]
+cities[0]  # Los Angelas
+cities[-1]  # Tokyo
+
+developer = "Jessica"
+list(developer)  # ['J', 'e', 's', 's', 'i', 'c', 'a']
+
+numbers = [1, 2, 3, 4, 5]
+len(numbers)  # 5
+
+programming_languages = ["Python", "Java", "C++", "Rust"]
+programming_languages[0] = "JavaScript"
+print(programming_languages)  # ['JavaScript', 'Java', 'C++', 'Rust']
