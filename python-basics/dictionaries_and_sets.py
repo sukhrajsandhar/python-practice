6e61c35592656831b6965ee92ed1f25dc2a51b56
sukhrajsandhar <sandharsukhraj5@gmail.com>
2026-03-30 22:09:54 -0700

    Dictionaries in Python

--- python-basics/dictionaries_and_sets.py
+++ python-basics/dictionaries_and_sets.py
@@ -1,1 +1,52 @@
-# hi
+# dictionary = {key1: value1, key2: value2}
+# Pizza example:
+pizza = {
+    "name": "Margherita Pizza",
+    "price": 8.9,
+    "calories_per_slice": 250,
+    "toppings": ["mozzarella", "basil"],
+}
+
+pizza = dict(
+    [
+        ("name", "Margherita Pizza"),
+        ("price", 8.9),
+        ("calories_per_slice", 250),
+        ("toppings", ["mozzarella", "basil"]),
+    ]
+)
+
+# dictionary[key]
+pizza["name"]
+# Output: 'Margherita Pizza'
+
+pizza["name"] = "Margherita"
+print(pizza["name"])  # 'Margherita'
+
+# dictionary.get(key, default)
+
+pizza.get("toppings", [])  # ['mozzarella', 'basil']
+
+pizza.keys()
+# dict_keys(['name', 'price', 'calories_per_slice'])
+
+pizza.values()
+# dict_values(['Margherita Pizza', 8.9, 250])
+
+pizza.items()
+# dict_items([('name', 'Margherita Pizza'), ('price', 8.9), ('calories_per_slice', 250)])
+
+pizza.pop("price", 10)
+# pizza.pop("total_price")  # KeyError
+
+pizza.popitem()  # In python 3.7 version removes the last inserted item.
+
+pizza.update({"price": 15, "total_time": 25})
+"""{
+    'name': 'Margherita Pizza',
+    'price': 15,
+    'calories_per_slice': 250,
+    'toppings': ['mozzarella', 'basil'],
+    'total_time': 25
+}"""
+# New updated dictionary
